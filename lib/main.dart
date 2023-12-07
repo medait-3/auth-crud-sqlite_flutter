e119b0e3e5eb8274759cfe0af2290c68149369fa
basirkhan1995 <basirkhan.hashemi@gmail.com>
2023-12-07 20:37:52 +0330

    error fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:quiz/Authtentication/login.dart';
-
 import '_constant/color.dart';
 
 void main() {
@@ -20,7 +19,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
